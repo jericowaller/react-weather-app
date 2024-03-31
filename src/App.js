67dc157ce794074050bbd945808208cb11e7fa58
Jerico <jericowaller@gmail.com>
2024-03-31 17:57:59 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({})
+  var location = ""
+  const API_KEY = { API_KEY }
+
+  const getData = (key, loc) => {
+    if (key === 'Enter') {
+      location = loc
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=imperial`
+      console.log(url)
+      fetch(url).then((response) => response.json())
+        .then((response) => {
+          setData(response)
+          console.log(response)
+        })
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-search">
+        <input
+          type='text'
+          onKeyDown={(key) => getData(key.key, key.target.value)}
+          placeholder={'Search for location...'}
+        />
+      </div>
+      {data.main && <div className='body'>
+        <div className="main-element-wrapper">
+          <div className="main-elements">
+            <div className="city">
+              <p>{data.name}</p>
+            </div>
+            <div className='main-temp'>
+              <p>{data.main.temp.toFixed()}°F</p>
+            </div>
+            <div className='feels'>
+              <p>Feels like: {data.main.feels_like.toFixed()}°F</p>
+            </div>
+          </div>
+        </div>
+        <div className='secondary-element-wrapper'>
+          <div className='secondary-elements'>
+            <div className='weather-type'>
+              <p>Current weather: {data.weather[0].main}</p>
+            </div>
+            <div className='humidity'>
+              <p>Humidity: {data.main.humidity}%</p>
+            </div>
+            <div className='wind-speed'>
+              <p>Wind speed: {data.wind.speed} MPH</p>
+            </div>
+            <div className=''></div>
+          </div>
+        </div>
+      </div>
+      }
     </div>
   );
 }
 
 export default App;
+
